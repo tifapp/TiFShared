5a864d814eb3a7d1d963d9da7f86a526112827c1
mhayes853 <matthewha853@gmail.com>
2024-03-19 21:40:55 -0700

    Metric conversions

---
+++ lib/MetricConversions.ts
@@ -0,0 +1,6 @@
+export const milesToMeters = (miles: number) => miles * METERS_PER_MILE
+export const milesToFeet = (miles: number) => miles * FEET_PER_MILE
+export const metersToMiles = (meters: number) => meters / METERS_PER_MILE
+
+const METERS_PER_MILE = 1609.344
+const FEET_PER_MILE = 5280
