8249eca45558897cbc44b0bf519c378c687c5a5a
mhayes853 <matthewha853@gmail.com>
2024-03-23 16:34:32 -0700

    Buff extension security

--- lib/Extend.test.ts
+++ lib/Extend.test.ts
@@ -1,4 +1,4 @@
-import { extension } from "./Extend"
+import { extension, protoypeExtension } from "./Extend"
 import "./Array"
 
 describe("Extension tests", () => {
@@ -20,12 +20,57 @@ describe("Extension tests", () => {
     expect(nums).toBe(nums.ext.ext.ext)
   })
 
-  it("should throw an error, if a forbidden extension name is used", () => {
+  it("should throw an error, if an insecure extension name is used", () => {
     const expectThrows = (name: string) => {
       expect(() => extension({}, { [name]: () => {} })).toThrow()
     }
-    expectThrows("__proto__")
     expectThrows("constructor")
     expectThrows("prototype")
+  })
+
+  it("should throw an error when trying to add an extension name that is already on the class", () => {
+    class Test {
+      foo() {}
+    }
+    expect(() => protoypeExtension(Test, { foo: () => {} })).toThrow()
+  })
+
+  it("should throw an error when trying to add a prototype extension name that is insecure", () => {
+    class Test2 {
+      foo() {}
+    }
+    const expectThrows = (name: string) => {
+      expect(() => protoypeExtension(Test2, { [name]: () => {} })).toThrow()
+    }
+    expectThrows("prototype")
+    expectThrows("constructor")
+  })
+
+  it("should throw an error when trying to override pre-existing extensions", () => {
+    class Test3 {}
+    protoypeExtension(Test3, { foo: () => {} })
+    expect(() => protoypeExtension(Test3, { foo: (t) => t })).toThrow()
+  })
+
+  it("should allow multiple prototype extensions", () => {
+    class Test4 {}
+    protoypeExtension(Test4, { foo: () => 1 })
+    protoypeExtension(Test4, { bar: () => 2 })
+    const t = new Test4() as any
+    expect(t.ext.foo()).toEqual(1)
+    expect(t.ext.bar()).toEqual(2)
+  })
+
+  it("should throw an error when trying to extend already existing properties on an object", () => {
+    expect(() => extension({ hello: "world" }, { hello: () => {} })).toThrow()
+  })
+
+  it("should throw an error when trying to extend already defined properties on an object", () => {
+    const obj = {}
+    Object.defineProperty(obj, "hello", {
+      writable: true,
+      value: "world"
+    })
+    expect(() => extension(obj, { hello: () => {} })).toThrow()
   })
 })
--- lib/Extend.ts
+++ lib/Extend.ts
@@ -7,11 +7,23 @@ export type Extension<
   [key in keyof Extensions]: OmitFirstArgument<Extensions[key]>
 }
 
-const insecureNameErrorMessage = (name: string) => {
-  return `${name} is an insecure extension name due to prototype pollution.`
+const INSECURE_NAMES = ["constructor", "prototype"]
+
+export class InsecureExtensionError extends Error {
+  constructor(name: string) {
+    super(`${name} is an insecure extension name due to prototype pollution.`)
+  }
 }
 
-const INSECURE_NAMES = ["__proto__", "constructor", "prototype"]
+export class ExtendedPropertyExistsError extends Error {
+  constructor(propertyName: string, clazz?: AnyClass) {
+    if (clazz) {
+      super(`${clazz.name} already contains a property named ${propertyName}.`)
+    } else {
+      super(`${propertyName} already exists on object.`)
+    }
+  }
+}
 
 /**
  * Returns a function to extend an existing class type with the supplied
@@ -36,9 +48,22 @@ export const extension = <
   value: Value,
   extensions: Extensions
 ) => {
+  return _extension(value, extensions, Object.getOwnPropertyNames(value))
+}
+
+const _extension = <
+  Value extends object,
+  Extensions extends Record<string, (instance: Value, ...args: any) => any>
+>(
+  value: Value,
+  extensions: Extensions,
+  preexistingNames: string[]
+) => {
   Object.keys(extensions).forEach((name) => {
     if (INSECURE_NAMES.includes(name)) {
-      throw new Error(insecureNameErrorMessage(name))
+      throw new InsecureExtensionError(name)
+    } else if (preexistingNames.includes(name)) {
+      throw new ExtendedPropertyExistsError(name)
     }
     Object.defineProperty(value, name, {
       writable: true,
@@ -47,6 +72,11 @@ export const extension = <
   })
   return value as Extension<Value, Extensions>
 }
+
+const table = new Map<
+  string,
+  Record<string, (instance: any, ...args: any) => any>
+>()
 
 /**
  * Applies an extension to the prototype of an object constructor to a property
@@ -74,9 +104,26 @@ export const protoypeExtension = <
   clazz: Class,
   extensions: Extensions
 ) => {
+  const properties = Object.keys(table.get(clazz.name) ?? {}).concat(
+    Object.getOwnPropertyNames(clazz.prototype)
+  )
+  Object.keys(extensions).forEach((name) => {
+    if (properties.includes(name)) {
+      throw new ExtendedPropertyExistsError(name, clazz)
+    } else if (INSECURE_NAMES.includes(name)) {
+      throw new InsecureExtensionError(name)
+    }
+  })
+  const allExtensions = { ...table.get(clazz.name), ...extensions }
+  table.set(clazz.name, allExtensions)
   Object.defineProperty(clazz.prototype, "ext", {
+    configurable: true,
     get() {
-      return extension(this, extensions)
+      return _extension(
+        this,
+        allExtensions,
+        Object.getOwnPropertyNames(clazz.prototype)
+      )
     }
   })
 }
