bbc13c0c81bdcba86374cdb4701c05702b464884
mhayes853 <matthewha853@gmail.com>
2024-04-05 23:03:53 -0700

    Update pollyfills

--- lib/Array.ts
+++ lib/Array.ts
@@ -1,15 +1,5 @@
 import { Extension, protoypeExtension } from "./Extend"
 
-// eslint-disable-next-line no-extend-native
-Array.prototype.with =
-  Array.prototype.with ||
-  function (index: number, element: any) {
-    return this.map((value: any, i: number) => {
-      if (index === i) return element
-      return value
-    })
-  }
-
 type BaseFunctions<Element> = Omit<
   ReturnType<typeof extensions<Element>>,
   "compactMap"
@@ -81,4 +71,21 @@ export const repeatElements = <T>(
     }
     return element
   })
+}
+
+/**
+ * Pollyfills {@link Array.prototype.with}.
+ */
+export const pollyfillArray = () => {
+  Array.prototype.with =
+    Array.prototype.with ||
+    function (index: number, element: any) {
+      if (index >= this.length || index < 0) {
+        throw new RangeError(`Invalid index : ${index}`)
+      }
+      return this.map((value: any, i: number) => {
+        if (index === i) return element
+        return value
+      })
+    }
 }
