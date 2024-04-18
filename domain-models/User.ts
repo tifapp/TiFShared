20e8315be9cad3302891b665050d2c59ca7ddf47
mhayes853 <matthewha853@gmail.com>
2024-04-17 20:41:38 -0700

    User Settings domain model

--- domain-models/User.ts
+++ domain-models/User.ts
@@ -100,6 +100,39 @@ export type BidirectionalUserRelations =
   | BlockedBidirectionalUserRelations
 
 /**
+ * A zod schema for {@link UserSettingsSchema}.
+ */
+export const UserSettingsSchema = z.object({
+  isAnalyticsEnabled: z.boolean(),
+  isCrashReportingEnabled: z.boolean(),
+  isEventNotificationsEnabled: z.boolean(),
+  isMentionsNotificationsEnabled: z.boolean(),
+  isChatNotificationsEnabled: z.boolean(),
+  isFriendRequestNotificationsEnabled: z.boolean(),
+  canShareArrivalStatus: z.boolean(),
+  updatedDateTime: z.date().nullable()
+})
+
+/**
+ * A type representing a user's settings.
+ */
+export type UserSettings = z.infer<typeof UserSettingsSchema>
+
+/**
+ * The default user settings, which enables all fields.
+ */
+export const DEFAULT_USER_SETTINGS = {
+  isAnalyticsEnabled: true,
+  isCrashReportingEnabled: true,
+  isEventNotificationsEnabled: true,
+  isMentionsNotificationsEnabled: true,
+  isChatNotificationsEnabled: true,
+  isFriendRequestNotificationsEnabled: true,
+  canShareArrivalStatus: true,
+  updatedDateTime: null
+} as const satisfies UserSettings
+
+/**
  * An reason that a user handle's raw text was unable to be parsed.
  */
 export type UserHandleParsingError = "bad-format" | "empty" | "too-long"
