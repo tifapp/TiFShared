ef9f14d3e08c1ebe3f884a3528a428df93c0ee11
tifapp <tifapppublic@gmail.com>
2024-07-19 17:48:50 -0700

    url utils file

--- lib/Middleware.test.ts
+++ lib/Middleware.test.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Middleware, chainMiddleware, runMiddleware } from "./Middleware"
-=======
-import { Middleware, chainMiddleware, handleMiddleware } from "./Middleware"
->>>>>>> e7d1d5a (no message)
 import { NonEmptyArray } from "./Types/HelperTypes"
 
 describe("MiddlewareTests", () => {
@@ -28,21 +24,13 @@ describe("MiddlewareTests", () => {
   })
   
   test("handle middlewares", async () => {
-<<<<<<< HEAD
     const middleware = runMiddleware(testMiddleware(1), testMiddleware(2), async (_) => _)
-=======
-    const middleware = handleMiddleware(testMiddleware(1), testMiddleware(2), async (_) => _)
->>>>>>> e7d1d5a (no message)
     const response = await middleware("input ")
     expect(response).toEqual("input test1 test2 ")
   })
 
   test("handle incomplete middlewares", async () => {
-<<<<<<< HEAD
     const middleware = runMiddleware(testMiddleware(1), testMiddleware(2))
-=======
-    const middleware = handleMiddleware(testMiddleware(1), testMiddleware(2))
->>>>>>> e7d1d5a (no message)
     expect(async () => middleware("input ")).rejects.toThrow("Middleware chain does not handle the request fully.")
   })
 })
