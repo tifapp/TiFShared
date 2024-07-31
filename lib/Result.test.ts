55cf9f45e30c4032e1c490ab5bc449683a952dc6
tifapp <tifapppublic@gmail.com>
2024-07-30 17:43:55 -0700

    add literal type

--- lib/Result.test.ts
+++ lib/Result.test.ts
@@ -1,157 +1,157 @@
 import { failure, promiseResult, success } from "./Result"
 
 describe("Result tests", () => {
-  const successResult = { status: "success", value: "passed" } as const
-  const failureResult = { status: "failure", value: "failed"} as const
+  const successResult = { status: "success", value: "passed" }
+  const failureResult = { status: "failure", value: "failed"}
 
   it("should construct a valid success result", () => {
-    expect(success("passed" as const)).toMatchObject(successResult)
+    expect(success("passed")).toMatchObject(successResult)
   })
 
   it("should observe success results without modifying them", () => {
-    const currentSuccess = success("passed" as const)
+    const currentSuccess = success("passed")
     currentSuccess.observe((result) => expect(result).toBe("passed"))
     expect(currentSuccess).toMatchObject(successResult)
   })
 
   it("should allow success results to map to promise results", async () => {
-    const promiseSuccess = success("passed" as const).flatMapSuccess(() =>
-      promiseResult(failure("failed" as const))
+    const promiseSuccess = success("passed").flatMapSuccess(() =>
+      promiseResult(failure("failed"))
     )
     expect(await promiseSuccess).toMatchObject(failureResult)
   })
   
   it("should allow success results to pass through a successful result", async () => {
-    const promiseSuccess = promiseResult(success("originalSuccess" as const)).passthroughSuccess(() =>
-      success("conditional" as const)
+    const promiseSuccess = promiseResult(success("originalSuccess")).passthroughSuccess(() =>
+      success("conditional")
     )
 
     expect(await promiseSuccess).toMatchObject({
-      status: "success" as const,
-      value: "originalSuccess" as const
+      status: "success",
+      value: "originalSuccess"
     })
   })
   
   it("should not allow success results to pass through a failing result", async () => {
-    const promiseSuccess = promiseResult(success("originalSuccess" as const)).passthroughSuccess(() =>
-      failure("conditional" as const)
+    const promiseSuccess = promiseResult(success("originalSuccess")).passthroughSuccess(() =>
+      failure("conditional")
     )
 
     expect(await promiseSuccess).toMatchObject({
-      status: "failure" as const,
-      value: "conditional" as const
+      status: "failure",
+      value: "conditional"
     })
   })
 
   it("should allow success results to be transformed", () => {
-    const currentSuccess = success("passed" as const)
-    expect(currentSuccess.withSuccess("newSuccess" as const)).toMatchObject({
+    const currentSuccess = success("passed")
+    expect(currentSuccess.withSuccess("newSuccess")).toMatchObject({
       status: "success",
       value: "newSuccess"
     })
-    expect(currentSuccess.mapSuccess(() => "mappedSuccess" as const)).toMatchObject({
+    expect(currentSuccess.mapSuccess(() => "mappedSuccess")).toMatchObject({
       status: "success",
       value: "mappedSuccess"
     })
     expect(
-      currentSuccess.flatMapSuccess(() => failure("failed" as const))
+      currentSuccess.flatMapSuccess(() => failure("failed"))
     ).toMatchObject(failureResult)
     expect(currentSuccess.inverted()).toMatchObject({
-      status: "failure" as const,
-      value: "passed" as const
+      status: "failure",
+      value: "passed"
     })
   })
 
   it("should not modify success result with failures", () => {
     expect(
-      success("passed" as const)
-        .withFailure("failed" as const)
-        .mapFailure(() => "failed" as const)
-        .flatMapFailure(() => failure("failed" as const))
-        .passthroughFailure(() => failure("failed" as const))
+      success("passed")
+        .withFailure("failed")
+        .mapFailure(() => "failed")
+        .flatMapFailure(() => failure("failed"))
+        .passthroughFailure(() => failure("failed"))
     ).toMatchObject(successResult)
   })
 
   it("should construct a valid failure result", () => {
-    expect(failure("failed" as const)).toMatchObject(failureResult)
+    expect(failure("failed")).toMatchObject(failureResult)
   })
 
   it("should observe failure results without modifying them", () => {
-    const currentFailure = failure("failed" as const)
+    const currentFailure = failure("failed")
     currentFailure.observe((result) => expect(result).toBe("failed"))
     expect(currentFailure).toMatchObject(failureResult)
   })
 
   it("should allow failure results to map to promise results", async () => {
-    const promiseFailure = failure("failed" as const).flatMapFailure(() =>
-      promiseResult(success("passed" as const))
+    const promiseFailure = failure("failed").flatMapFailure(() =>
+      promiseResult(success("passed"))
     )
     expect(await promiseFailure).toMatchObject(successResult)
   })
   
   it("should allow failure results to pass through a successful result", async () => {
-    const promiseFailure = promiseResult(failure("originalFailure" as const)).passthroughFailure(() =>
-      success("conditional" as const)
+    const promiseFailure = promiseResult(failure("originalFailure")).passthroughFailure(() =>
+      success("conditional")
     )
 
     expect(await promiseFailure).toMatchObject({
-      status: "failure" as const,
+      status: "failure",
       value: "originalFailure"
     })
   })
   
   it("should not allow failure results to pass through a failing result", async () => { 
-    const promiseFailure = promiseResult(failure("originalFailure" as const)).passthroughFailure(() =>
-      failure("conditional" as const)
+    const promiseFailure = promiseResult(failure("originalFailure")).passthroughFailure(() =>
+      failure("conditional")
     )
 
     expect(await promiseFailure).toMatchObject({
-      status: "failure" as const,
+      status: "failure",
       value: "conditional"
     })
   })
 
   it("should allow failure results to be transformed", () => {
-    const currentFailure = failure("failed" as const)
+    const currentFailure = failure("failed")
     expect(currentFailure.withFailure("newFailure")).toMatchObject({
-      status: "failure" as const,
+      status: "failure",
       value: "newFailure"
     })
     expect(currentFailure.mapFailure(() => "mappedFailure")).toMatchObject({
-      status: "failure" as const,
+      status: "failure",
       value: "mappedFailure"
     })
     expect(
-      currentFailure.flatMapFailure(() => success("passed" as const))
+      currentFailure.flatMapFailure(() => success("passed"))
     ).toMatchObject(successResult)
     expect(currentFailure.inverted()).toMatchObject({
-      status: "success" as const,
-      value: "failed" as const
+      status: "success",
+      value: "failed"
     })
   })
 
   it("should not modify failure result with successes", () => {
     expect(
-      failure("failed" as const)
-        .withSuccess("passed" as const)
-        .mapSuccess(() => "passed" as const)
-        .flatMapSuccess(() => failure("passed" as const))
-        .passthroughSuccess(() => failure("passed" as const))
+      failure("failed")
+        .withSuccess("passed")
+        .mapSuccess(() => "passed")
+        .flatMapSuccess(() => failure("passed"))
+        .passthroughSuccess(() => failure("passed"))
     ).toMatchObject(failureResult)
   })
 
   it("should construct a valid promise result", async () => {
-    expect(await promiseResult((async () => success("passed" as const))())).toMatchObject(successResult)
-    expect(await promiseResult((async () => failure("failed" as const))())).toMatchObject(failureResult)
+    expect(await promiseResult((async () => success("passed"))())).toMatchObject(successResult)
+    expect(await promiseResult((async () => failure("failed"))())).toMatchObject(failureResult)
   })
 
   it("should allow nested promise results", async () => {
-    expect(await promiseResult(promiseResult(success("passed" as const)))).toMatchObject(successResult)
-    expect(await promiseResult(promiseResult(failure("failed" as const)))).toMatchObject(failureResult)
+    expect(await promiseResult(promiseResult(success("passed")))).toMatchObject(successResult)
+    expect(await promiseResult(promiseResult(failure("failed")))).toMatchObject(failureResult)
   })
 
   it("should observe promise results without modifying them", async () => {
-    const currentFailure = promiseResult(failure("failed" as const))
+    const currentFailure = promiseResult(failure("failed"))
     currentFailure.observe((result) => expect(result).toBe("failed"))
     const result = await currentFailure
     expect(result).toMatchObject(failureResult)
@@ -159,7 +159,7 @@ describe("Result tests", () => {
 
   it("should should not observe failure result when successful", async () => {
     const observeFn = jest.fn()
-    let currentSuccess = promiseResult(success("passed" as const))
+    let currentSuccess = promiseResult(success("passed"))
     currentSuccess = currentSuccess.observeFailure(observeFn)
     await currentSuccess
     expect(observeFn).not.toHaveBeenCalled()
@@ -167,16 +167,16 @@ describe("Result tests", () => {
 
   it("should should observe failure without modifying current result", async () => {
     const observeFn = jest.fn()
-    const result1 = promiseResult(failure("failed" as const))
+    const result1 = promiseResult(failure("failed"))
     const result2 = result1.observeFailure(observeFn)
     await result2
     expect(result1).toBe(result2)
-    expect(observeFn).toHaveBeenCalledWith("failed" as const)
+    expect(observeFn).toHaveBeenCalledWith("failed")
   })
 
   it("should should not observe success result when failure", async () => {
     const observeFn = jest.fn()
-    let currentFailure = promiseResult(failure("failed" as const))
+    let currentFailure = promiseResult(failure("failed"))
     currentFailure = currentFailure.observeSuccess(observeFn)
     await currentFailure
     expect(observeFn).not.toHaveBeenCalled()
@@ -184,38 +184,38 @@ describe("Result tests", () => {
 
   it("should should observe failure without modifying current result", async () => {
     const observeFn = jest.fn()
-    const result1 = promiseResult(success("passed" as const))
+    const result1 = promiseResult(success("passed"))
     const result2 = result1.observeSuccess(observeFn)
     await result2
     expect(result1).toBe(result2)
-    expect(observeFn).toHaveBeenCalledWith("passed" as const)
+    expect(observeFn).toHaveBeenCalledWith("passed")
   })
   
   it("should allow promise results to be unwrapped", async () => {
-    expect(await promiseResult(failure("failed" as const)).unwrap()).toBe("failed")
-    expect(await promiseResult(success("passed" as const)).unwrap()).toBe("passed")
+    expect(await promiseResult(failure("failed")).unwrap()).toBe("failed")
+    expect(await promiseResult(success("passed")).unwrap()).toBe("passed")
   })
 
   it("should allow promise results to be transformed", async () => {
-    const currentResult = promiseResult(failure("failed" as const))
+    const currentResult = promiseResult(failure("failed"))
     expect(await currentResult.withFailure("newFailure")).toMatchObject({
-      status: "failure" as const,
+      status: "failure",
       value: "newFailure"
     })
     expect(await currentResult.mapFailure(() => "mappedFailure")).toMatchObject(
-      { status: "failure" as const, value: "mappedFailure" }
+      { status: "failure", value: "mappedFailure" }
     )
     expect(
-      await currentResult.flatMapFailure(() => success("passed" as const))
+      await currentResult.flatMapFailure(() => success("passed"))
     ).toMatchObject(successResult)
     expect(
       await currentResult.inverted().withSuccess("newSuccess")
-    ).toMatchObject({ status: "success" as const, value: "newSuccess" })
+    ).toMatchObject({ status: "success", value: "newSuccess" })
     expect(
       await currentResult.inverted().mapSuccess(() => "mappedSuccess")
-    ).toMatchObject({ status: "success" as const, value: "mappedSuccess" })
+    ).toMatchObject({ status: "success", value: "mappedSuccess" })
     expect(
-      await currentResult.inverted().flatMapSuccess(() => failure("failed" as const))
+      await currentResult.inverted().flatMapSuccess(() => failure("failed"))
     ).toMatchObject(failureResult)
   })
 })
---
+++ lib/Types/LiteralType.ts
@@ -0,0 +1,28 @@
+type Primitive = string | number | boolean | null | undefined;
+
+type InferLiteral<T> = T extends Primitive ? T : never;
+
+/**
+ * Recursively infers the most specific literal type for given input.
+ * For primitives, it directly infers the literal type.
+ * For arrays, it infers each element.
+ * For objects, it infers each property.
+ * 
+ * Ex.
+ * ```ts
+ * declare function literal<T>(options: Literal<T>): T;
+ * const complexObject = literal({x: {y: 3, z: [1, 2, 3]}, w: new Date(2000)})
+ * // typeof complexObject is inferred the literal values
+ * ```
+ */
+export type Literal<T> = T extends Primitive ? InferLiteral<T> :
+  T extends Array<infer U> ? Array<Literal<U>> :
+  {
+    [P in keyof T]-?: Literal<T[P]>;
+  };
+
+// Example Usage:
+declare function literal<T>(options: Literal<T>): T;
+
+// typeof complexObject is inferred with literal values
+const complexObject = literal({x: {y: 3, z: [1, 2, 3]}, w: new Date(2000)})
