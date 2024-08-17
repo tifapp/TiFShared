eeed1e882a6965c08c2b7ea702262f03e4ecb811
tifapp <tifapppublic@gmail.com>
2024-08-17 00:39:59 -0700

    import zod extensions before handles

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import "./domain-models/Event"
-import "./domain-models/User"
 import { pollyfillArray } from "./lib/Array"
 import "./lib/Date"
 import "./lib/Dayjs"
 import "./lib/Math"
 import "./lib/Zod"
+
+import "./domain-models/Event"
+import "./domain-models/User"
 
 pollyfillArray()
