5258f71a5809bf184f746f9cada62d9b6d3800d6
mhayes853 <matthewha853@gmail.com>
2024-09-06 22:09:32 -0700

    Update EventEditLocation

--- domain-models/Event.ts
+++ domain-models/Event.ts
@@ -183,9 +183,18 @@ export type TrackableEventArrivalRegions = z.rInfer<
   typeof TrackableEventArrivalRegionsSchema
 >
 
+const EventEditCoordinateSchema = z.object({
+  type: z.literal("coordinate"),
+  value: LocationCoordinate2DSchema
+})
+const EventEditPlacemarkSchema = z.object({
+  type: z.literal("placemark"),
+  value: PlacemarkSchema
+})
+
 export const EventEditLocationSchema = z.union([
-  PlacemarkSchema,
-  LocationCoordinate2DSchema
+  EventEditCoordinateSchema,
+  EventEditPlacemarkSchema
 ])
 
 /**
