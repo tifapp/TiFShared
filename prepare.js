40284b6f3ca230b18cdd850984caeb91cd9de14b
tifapp <tifapppublic@gmail.com>
2024-09-20 00:16:44 -0700

    no message

--- prepare.js
+++ prepare.js
@@ -1,4 +1,4 @@
-if (process.env.TIFSHARED_TS !== 'true') { 
+if (process.env.TIFSHARED_JS === 'true') { 
   require('child_process').execSync('npm run build', { stdio: 'inherit', env: process.env })
-  console.log('Built js package. If you would like to see the typescript version of the package, reinstall with the TIFSHARED_TS env variable set to true.')
+  console.log('Built js package. If you would like to see the typescript version of the package, reinstall with the TIFSHARED_JS env variable set to false.')
 }
