01524898c5e6d4f72db4f8d3d585a77d600633f3
tifapp <tifapppublic@gmail.com>
2024-09-30 17:22:36 -0700

    zod adjustment (+1 squashed commits)
    Squashed commits:
    
    [9b3b68e] Zod optional parseable update. Allows zod errors to pass through and allows multiple error messages per optionalParseable schema.

--- domain-models/FixedDateRange.test.ts
+++ domain-models/FixedDateRange.test.ts
@@ -17,40 +17,40 @@ describe("FixedDateRange tests", () => {
     expect(range).toBeUndefined()
   })
 
-  test("moveStartDate basic", () => {
+  test("moveStartDateTime basic", () => {
     const range = dateRange(
       new Date("2023-02-25T00:17:00"),
       new Date("2023-02-25T00:18:00")
-    )?.moveStartDateWithAutocorrection(new Date(0))
-    expect(range?.startDate).toEqual(new Date(0))
-    expect(range?.endDate).toEqual(new Date("2023-02-25T00:18:00"))
+    )?.moveStartWithAutocorrection(new Date(0))
+    expect(range?.startDateTime).toEqual(new Date(0))
+    expect(range?.endDateTime).toEqual(new Date("2023-02-25T00:18:00"))
   })
 
-  test("moveEndDate basic", () => {
-    const newEndDate = new Date("3000-01-01T00:00:00")
+  test("moveEndDateTime basic", () => {
+    const newEndDateTime = new Date("3000-01-01T00:00:00")
     const range = dateRange(
       new Date("2023-02-25T00:17:00"),
       new Date("2023-02-25T00:18:00")
-    )?.moveEndDateWithAutocorrection(newEndDate)
-    expect(range?.startDate).toEqual(new Date("2023-02-25T00:17:00"))
-    expect(range?.endDate).toEqual(newEndDate)
+    )?.moveEndWithAutocorrection(newEndDateTime)
+    expect(range?.startDateTime).toEqual(new Date("2023-02-25T00:17:00"))
+    expect(range?.endDateTime).toEqual(newEndDateTime)
   })
 
   it("moving start date past end date moves end date past the start date by the previous interval between the dates", () => {
     const range = dateRange(
       new Date("2023-02-25T00:17:00"),
       new Date("2023-02-25T00:18:00")
-    )?.moveStartDateWithAutocorrection(new Date("2023-02-25T00:19:00"))
+    )?.moveStartWithAutocorrection(new Date("2023-02-25T00:19:00"))
     // NB: The previous interval was 1 minute, so we ensure the end date is 1 minute ahead of the start date
-    expect(range?.endDate).toEqual(new Date("2023-02-25T00:20:00"))
+    expect(range?.endDateTime).toEqual(new Date("2023-02-25T00:20:00"))
   })
 
   it("moving end date before start date moves start date before the end date by the previous interval between the dates", () => {
     const range = dateRange(
       new Date("2023-02-25T00:17:00"),
       new Date("2023-02-25T00:18:00")
-    )?.moveEndDateWithAutocorrection(new Date("2023-02-25T00:16:00"))
+    )?.moveEndWithAutocorrection(new Date("2023-02-25T00:16:00"))
     // NB: The previous interval was 1 minute, so we ensure the start date is 1 minute behind of the end date
-    expect(range?.startDate).toEqual(new Date("2023-02-25T00:15:00"))
+    expect(range?.startDateTime).toEqual(new Date("2023-02-25T00:15:00"))
   })
 })
