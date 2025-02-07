24f260a35bf4323287ce919480b63a293bdca98e
mhayes853 <matthewha853@gmail.com>
2025-02-07 11:45:00 -0800

    Request logging middleware

--- api/TransportMiddleware.test.ts
+++ api/TransportMiddleware.test.ts
@@ -1,5 +1,6 @@
+import { addLogHandler, consoleLogHandler, resetLogHandlers } from "../logging"
 import { ClientExtensions } from "./Transport"
-import { jwtMiddleware } from "./TransportMiddleware"
+import { jwtMiddleware, requestLoggingMiddleware } from "./TransportMiddleware"
 import { TiFAPIInputContext } from "./TransportTypes"
 
 describe("TiFAPITransportMiddlewareTests", () => {
@@ -9,7 +10,10 @@ describe("TiFAPITransportMiddlewareTests", () => {
     it("should include a bearer token in the request headers with the jwt", async () => {
       const middleware = jwtMiddleware(async () => TEST_JWT)
       const next = jest.fn()
-      await middleware({ headers: {} } as TiFAPIInputContext<ClientExtensions>, next)
+      await middleware(
+        { headers: {} } as TiFAPIInputContext<ClientExtensions>,
+        next
+      )
       expect(next).toHaveBeenCalledWith(
         expect.objectContaining({
           headers: { Authorization: `Bearer ${TEST_JWT}` }
@@ -20,10 +24,38 @@ describe("TiFAPITransportMiddlewareTests", () => {
     it("should omit the bearer token in the request headers when no jwt", async () => {
       const middleware = jwtMiddleware(async () => undefined)
       const next = jest.fn()
-      await middleware({ headers: {} } as TiFAPIInputContext<ClientExtensions>, next)
+      await middleware(
+        { headers: {} } as TiFAPIInputContext<ClientExtensions>,
+        next
+      )
       expect(next).toHaveBeenCalledWith(
         expect.objectContaining({ headers: {} })
       )
+    })
+  })
+
+  describe("LoggingMiddleware tests", () => {
+    test("logs a request", async () => {
+      const testHandler = jest.fn()
+      addLogHandler(consoleLogHandler())
+      addLogHandler(testHandler)
+      const middleware = requestLoggingMiddleware("test", "info")
+      const next = jest.fn()
+      await middleware(
+        {
+          headers: {},
+          endpointName: "/test",
+          endpointSchema: {
+            httpRequest: {
+              method: "POST"
+            }
+          }
+        } as TiFAPIInputContext<ClientExtensions>,
+        next
+      )
+      expect(testHandler).toHaveBeenCalledTimes(1)
+      expect(next).toHaveBeenCalledTimes(1)
+      resetLogHandlers()
     })
   })
 })
